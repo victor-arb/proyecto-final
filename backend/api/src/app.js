a994f87e7336e4b8784670258d668dd19bc6d434
Victor Arbelaez <victorarbelaez6@gmail.com>
2024-11-01 12:52:56 -0500

    Se crea el app.js que integra los demas modulos

--- backend/api/src/app.js
+++ backend/api/src/app.js
@@ -1,31 +1,38 @@
 import express from "express";
 import { setContentType } from "./middlewares/middleware.mjs";
-import { CourseRoutes } from "./routes/CourseRoutes.mjs";
-import { DegreeRoutes } from "./routes/DegreeRoutes.mjs";
-import { FacultyRoutes } from "./routes/FacultyRoutes.mjs";
-import { ProfessorRoutes } from "./routes/ProfessorRoutes.mjs";
-import { StudentRoutes } from "./routes/StudentRoutes.mjs";
+import { UsuarioRoutes } from "./routes/UsuarioRoutes.mjs";
+import { MedicoRoutes } from "./routes/MedicoRoutes.mjs";
+import { EspecialidadRoutes } from "./routes/EspecialidadRoutes.mjs";
+import { HorarioRoutes } from "./routes/HorarioRoutes.mjs";
+import { CitaRoutes } from "./routes/CitaRoutes.mjs";
+import { ServicioAdicionalRoutes } from "./routes/ServicioAdicionalRoutes.mjs";
 
 const app = express();
 app.use(express.json());
 app.use(setContentType);
 
-const courseRoutes = new CourseRoutes();
-const degreeRoutes = new DegreeRoutes();
-const facultyRoutes = new FacultyRoutes();
-const professorRoutes = new ProfessorRoutes();
-const studentRoutes = new StudentRoutes();
+// Instancia de rutas para cada entidad
+const usuarioRoutes = new UsuarioRoutes();
+const medicoRoutes = new MedicoRoutes();
+const especialidadRoutes = new EspecialidadRoutes();
+const horarioRoutes = new HorarioRoutes();
+const citaRoutes = new CitaRoutes();
+const servicioAdicionalRoutes = new ServicioAdicionalRoutes();
 
-app.use("/courses", courseRoutes.router);
-app.use("/degrees", degreeRoutes.router);
-app.use("/faculties", facultyRoutes.router);
-app.use("/professors", professorRoutes.router);
-app.use("/students", studentRoutes.router);
+// Mapeo de rutas
+app.use("/usuarios", usuarioRoutes.router);
+app.use("/medicos", medicoRoutes.router);
+app.use("/especialidades", especialidadRoutes.router);
+app.use("/horarios", horarioRoutes.router);
+app.use("/citas", citaRoutes.router);
+app.use("/servicios-adicionales", servicioAdicionalRoutes.router);
 
+// Manejo de rutas inexistentes
 app.all("*", (req, res) => {
-  res.status(404).send(JSON.stringify({ message: "NO existe esa ruta" }));
+  res.status(404).send(JSON.stringify({ message: "No existe esa ruta" }));
 });
 
+// Inicialización del servidor
 app.listen(8080, () => {
   console.log("Servidor escuchando en el puerto 8080");
 });
